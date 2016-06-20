a6aa32e82785a128940b74740de9457494fd753b
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-20 18:36:22 +0000

    Resubmitting python basic prog due to edit on 2_uppercases_all_letters.py

--- test_2_uppercases_all_letters.py
+++ test_2_uppercases_all_letters.py
@@ -1,6 +1,6 @@
 def uppercases_all_letters(str):
-  return str.upper
-
+  return str.upper()
+  
 # -----------------------------------------------------------------------------
 
 # These are the automated tests for this exercise. Do not modify them at all.
